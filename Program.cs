522b516b3a7e049ceb55845cfaf197fff814b2be
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-11 20:26:12 -0300

    Resolvi a atividade

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write(@"O que é, o que é?");
+Thread.Sleep(1000);
+Console.WriteLine();
+Console.Write("Tem cabeça e tem dente, não é bicho e nem é gente.");
+Thread.Sleep(5000);
+Console.WriteLine();
+Console.Write(@"Alho");
+
